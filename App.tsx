87d7b0faffa26d49f0a2347b226fbe04e0198ba7
Flávia Schuenquer <flavia_schuenquer@hotmail.com>
2020-12-09 20:37:59 -0200

    Adicionando aplicação front-mobile e React native

--- App.tsx
+++ App.tsx
@@ -1,21 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+import { useFonts, Play_400Regular, Play_700Bold} from '@expo-google-fonts/play';
+
+import Header from './src/components/Header';
+import Home from './src/pages/Home';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Play_400Regular, 
+    Play_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading/>
+  }else {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Header/>
+      <Home/>
       <StatusBar style="auto" />
     </View>
   );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#180F49'
+   
   },
+
 });
